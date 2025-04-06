7060f75f214d826a5d5bbef9f0f2e6fe04410478
pedrovandrade <pedrovandrade@gmail.com>
2025-04-06 21:58:49 +0200

    Update favicon.ico and metadata description

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import ThreeDModelPage from "~/components/ThreeDModelPage";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "KZS Talks - 3D model" },
+    { name: "description", content: "Sample code to display 3D model for the Kaizen Talks!" },
   ];
 }
 
